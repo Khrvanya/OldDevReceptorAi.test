7aabf754ad8ce39a224de9284b23b5018cdb61f8
Ivan <61050608+Khrvanya@users.noreply.github.com>
2024-03-18 14:06:45 +0200

    Update login_page.py

--- page_names/login_page.py
+++ page_names/login_page.py
@@ -12,15 +12,15 @@ class LoginPage:
         self.__password_field = None
 
     def get_sign_in_button(self):
-        self.__sign_in_button = Button(self._driver.find_element(By.XPATH, "//*[@value='sign in'] "))
+        self.__sign_in_button = Button(self.driver.find_element(By.XPATH, "//*[@value='sign in'] "))
         return self.__sign_in_button
 
     def get_email_field(self):
-        self.__email_field = TextBox(self._driver.find_element(By.ID, "username"))
+        self.__email_field = TextBox(self.driver.find_element(By.ID, "username"))
         return self.__email_field
 
     def get_password_field(self):
-        self.__password_field = TextBox(self._driver.find_element(By.ID, "password"))
+        self.__password_field = TextBox(self.driver.find_element(By.ID, "password"))
         return self.__password_field
 
 
